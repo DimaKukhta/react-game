adeb54b8d0f52645f799e4777af3f36aa55cedcc
Dima Kukhta <dima.kukhta.2001@mail.ru>
2021-03-03 20:58:05 +0300

    fix: clean folder of project

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
     return (
         <div className="header">
             <NavLink to="/menu"><img className="back-arrow" src="https://img.icons8.com/carbon-copy/100/000000/circled-left.png"/></NavLink>
-            <img className="back-arrow" src="https://img.icons8.com/wired/64/000000/full-screen.png" />
+            <img className="full-screen" src="https://img.icons8.com/wired/64/000000/full-screen.png" />
         </div>
     )
 }
